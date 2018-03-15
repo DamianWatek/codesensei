517a2019ca34373abbca99dbe5b8cbc66b92e0d6
Damian Watroba <watek-177@o2.pl>
2018-03-15 22:39:21 +0100

    panggram

--- part II/pangram/pangram.rb
+++ part II/pangram/pangram.rb
@@ -2,11 +2,19 @@
 # Napisz metodę, która sprawdzi czy podany ciąg znaków jest pangramem dla języka
 # polskiego.
 # Np:
-
-def pangram?
-  range = ('a'..'z').to_a
-  puts range
+require "pry"
+def pangram?(string)
+  range = ('a'..'z').to_a + [ 'ą', 'ć', 'ę', 'ł', 'ń', 'ó', 'ś', 'ź', 'ż' ] - ["q", "x", "v"]
+  array = []
+  range.each do |i|
+    if (string.downcase).include?(i)
+      array << i
+    end
+  end
+  range == array
 end
-pangram?
-# pangram?('Dość błazeństw, żrą mój pęk luźnych fig') #=> true
-# pangram?('Losowy ciąg znaków') #=> false
+
+puts pangram?('Dość błazeństw, żrą mój pęk luźnych fig') #=> true
+puts pangram?('Losowy ciąg znaków') #=> false
+puts pangram?('Mknął boży puch. Jeść stóg z lędźwi Frań?') 
+puts pangram?('W Mińsku lżą naftę Jóź. Gość brzydł. Pech!') 
