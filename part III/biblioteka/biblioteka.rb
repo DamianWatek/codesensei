cb918f7f66e64c88e544024e6c08ac51d77dad12
Damian Watroba <watek-177@o2.pl>
2018-04-11 21:31:06 +0200

    do sprawdzenia

--- part III/biblioteka/biblioteka.rb
+++ part III/biblioteka/biblioteka.rb
@@ -1,19 +1,7 @@
-# Napisz program do obsługi biblioteki. Powinien on umożliwiać:
-#   • wyświetlenie listy książek w bibliotece, z zaznaczeniem dostępnych oraz aktualnie
-#   wypożyczonych (lista książek nie powinna być udostępniona publicznie)
-#   • dodawanie nowych książek do listy dostępnych w bibliotece GOTOWE
-#   • wypożyczanie i oddawanie książek przez czytelników (niemożliwe jest
-#   wypożyczenie już wypożyczonej książki)
-#   • dodawanie nowych czytelników do biblioteki wraz z generowaniem im numeru karty GOTOWE
-#   bibliotecznej (powinien on składać się z liter oraz cyfr i być unikalny dla każdego
-#   czytelnika)
-#   • wyświetlenie listy książek wypożyczonych w danym momencie przez konkretnego
-#   czytelnika, wraz z datami wypożyczenia każdej z nich
+
+#   
 #   • wyświetlenie historii wypożyczeń danego czytelnika wraz z czasem
 #   przetrzymywania każdej z nich (uwzględniającej oddane już książki)
-
-# Stwórz ok. 10 książek i 3 czytelników. Sprawdź manualnie poprawność działania
-# programu.
 
 # Klasa Biblioteka - metody wyświetlanie listy ksiązek(wypożyczone, dostepne), wyswietlanie informacji dot. czytelnika, historia czytelinika
 # Klasa Książka - dodawanie ksiazek
@@ -30,17 +18,17 @@ class Library
     @books = books
     @readers = {}
   end
-  
+  # Wyświetlanie listy książek
   def show_books_list
    @books.each do |key, value|
     puts "Tittle: #{key.tittle}, Author: #{key.author}, Available?: #{value}"
    end
   end
-  
+  # Dodawanie książek
   def add_books(tittle, author)
     @books[Book.new(tittle, author)] = true
   end
-  
+  # Dodawanie czytelnika
   def add_reader(reader)
     def cart_number
       cart_number = []
@@ -50,10 +38,34 @@ class Library
     end
     readers[reader] = cart_number
   end
-
+  # Wyświetlanie listy czytelników
   def show_readers_list
     @readers.each do |reader, cart|
       puts "Name: #{reader.name}, Age: #{reader.age}, Cart number: #{cart}"
+    end
+  end
+  # Szukanie książek 
+  def find_book(book_tittle, action)
+    if action == "borrow"
+      @books.each do |book, value|
+        if book_tittle == book.tittle && value == true
+          @books[book] = false
+          return book
+        elsif book_tittle == book.tittle && value == false
+          return false
+        end
+      end
+      nil
+    elsif action == "donating"
+      @books.each do |book, value|
+        if book_tittle == book.tittle && value == false
+          @books[book] = true
+          return "Dziękujemy za zwrot książki."
+        elsif book_tittle == book.tittle && value == true
+          return true
+        end
+      end
+      nil
     end
   end
 end
@@ -71,12 +83,46 @@ end
 class Reader < Library
   attr_reader :name
   attr_reader :age
+  attr_reader :my_books
+  attr_reader :history
 
   def initialize(name, age)
     @name = name
     @age = age
+    @my_books = []
+    @history = []
   end
-
+  # Wypożyczanie książek
+  def borrow_book(book_tittle, library)
+    book = library.find_book(book_tittle, "borrow")
+    if book == nil
+      puts "Nie ma takiej książki w biblitece"
+    elsif book == false
+      puts "Książka jest już wypożyczona"
+    else
+      @history << [ book.tittle, Time.new(2018, 3, 10, 15, 30) ] 
+      @my_books << [ book.tittle, Time.new(2018, 3, 10, 15, 30) ] 
+    end
+  end
+  # Oddawanie książek 
+  def donating_book(book_tittle, library)
+    @my_books.each do |my_book|
+      donating = library.find_book(book_tittle, "donating")
+      if my_book[0] == book_tittle
+        puts donating
+        @history.delete(my_book)
+        my_book << Time.now
+        @history << my_book
+        @my_books.delete(my_book)
+      elsif donating == nil
+        puts "Nie mieliśmy takiej książki"
+      elsif donating == true
+        puts "Mamy już taką książkę, czy napewno była wypożyczana u nas?"
+      else
+        puts "Nie masz takiej ksiązki do oddania"
+      end
+    end
+  end
 end
 # Tworzenie książek
 book_1 = Book.new("Mały książę", "Antoine de Saint-Exupéry")
@@ -118,4 +164,26 @@ library.add_reader(olga)
 library.add_reader(pawel)
 # Pokazujemy aktualną liste czytelników
 library.show_readers_list
+
+damian.borrow_book("Mały książę", library)
+damian.borrow_book("Kod Leonarda da Vinci", library)
+puts ""
+olga.borrow_book("Kosogłos", library)
+olga.borrow_book("Mały książę", library)
+olga.borrow_book("Charlie i fabryka czekolady", library)
+olga.borrow_book("Hamlet", library)
+
+puts ""
+puts damian.my_books
+puts ''
+puts olga.my_books
+puts ""
+
+damian.donating_book("Mały książę", library)
+damian.donating_book("Kosogłos", library)
+damian.donating_book("Hamlet", library)
+puts damian.my_books
+
+library.show_books_list
+puts damian.history.to_s
 
